fdab34a98753b0a44302a8830f000ee8c7926af9
robertogms1 <robertogms17@gmail.com>
2023-04-07 12:34:31 -0300

    interface

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class InterFace(App):
 # Adiciona a imagem como widget de fundo
         Fundo = Image(source='Fundo.jpg', allow_stretch=True, keep_ratio=False)
         box.add_widget(Fundo)
-#informação titulo
+#informação titulo 
         label_title=Label(text='Insira as informações químicas do rótulo do vinho:', font_size=20, halign='center', pos_hint={'center_x': 0.5, 'top': 1.4}, color=(1, 0, 0, 1))
         box.add_widget(label_title)
 #informação de entrada
@@ -23,7 +23,7 @@ class InterFace(App):
 #caixa de entrada onde o usuario digita as informaçoes neste caso alcool
         entrada_alcool=TextInput(multiline=False, font_size=16, pos_hint={'center_x': 0.5, 'top': 0.7}, size_hint=(0.3, 0.05))
         box.add_widget(entrada_alcool)
-#botão de enviar       
+#botao de enviar       
         button= Button(text='Enviar', font_size=16, pos_hint={'center_x': 0.5, 'top': 0.3}, size_hint=(0.3, 0.1),color=(0, 0, 1, 1))
         box.add_widget(button)
 
